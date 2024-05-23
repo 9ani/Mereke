1e0ee88bf43eb646bcd1ef2bf68d199eb08a01e0
9ani <Gankon17@gmail.com>
2024-05-23 22:42:38 +0500

    update

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/mereke').then(() =>
-{
-    console.log('Connected to Mongodb')
-}).catch((e)=>
-{
-    console.log('Failed to connect to Mongodb')
+mongoose.connect('mongodb+srv://gani:qwerty123456@weatherapi.qd1uz2q.mongodb.net/mereke?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
 });
